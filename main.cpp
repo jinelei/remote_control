99e06125db7216862a694e6f245ac2f909769452
jinelei <jinelei@gmail.com>
2025-05-17 23:18:25 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "demo.hpp"
-#include "gpio.hpp"
+// #include "gpio.hpp"
 int main()
 {
 	printf("Remote controll is startup!!!\n");
 	test();
-	init_gpio();
+	// init_gpio();
 	// pc_startup();
 	printf("Remote controll is shutdown!!!\n");
 	return 0;
